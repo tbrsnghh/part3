87d6876297b58c31f6948f066140c2f7826ffba3
tbrsnghh <nhham1771@gmail.com>
2024-12-31 13:41:24 +0700

    hhh

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+const Person = require('./models/person')
 const cors = require('cors')
 const morgan = require('morgan')
 app.use(express.static('dist'))
@@ -15,32 +17,12 @@ app.use(morgan((tokens, req, res) => {
       JSON.stringify(req.body)       // Body of the request
     ].join(' ')                      
   }))
-let persons =
-[
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let persons = []
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p>
